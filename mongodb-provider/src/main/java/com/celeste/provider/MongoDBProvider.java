564962f7f34cbda7e8e4178381f29af1a63537d9
mluizaa00 <taeckls@gmail.com>
2021-03-04 13:12:28 -0300

    Updated MongoDBProvider.

--- mongodb-provider/src/main/java/com/celeste/provider/MongoDBProvider.java
+++ mongodb-provider/src/main/java/com/celeste/provider/MongoDBProvider.java
@@ -1,28 +1,34 @@
 package com.celeste.provider;
 
-import com.celeste.ConnectionProvider;
-import com.mongodb.*;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoCredential;
+import com.mongodb.ReadPreference;
+import com.mongodb.ServerAddress;
+import com.mongodb.MongoClient;
 import dev.morphia.Datastore;
 import dev.morphia.Morphia;
 import lombok.Getter;
 import lombok.SneakyThrows;
 import lombok.Synchronized;
-
+import com.celeste.ConnectionProvider;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
-@Getter(onMethod_= { @Synchronized } )
-public class MongoDBProvider implements ConnectionProvider<MongoClient> {
+@Getter(onMethod_= { @Synchronized })
+public final class MongoDBProvider implements ConnectionProvider<MongoClient> {
 
     private MongoClient client;
     private Morphia morphia;
     private Datastore datastore;
 
+    /**
+     * Connects into the MongoDB database with the credentials.
+     *
+     * @param properties Properties with provider credentials.
+     */
     @Override @Synchronized @SneakyThrows
-    public void connect(Properties properties) {
-        Class.forName("com.mongodb.MongoClient");
-
+    public void connect(final Properties properties) {
         final ServerAddress address = new ServerAddress(
             properties.getProperty("hostname"),
             Integer.parseInt(properties.getProperty("port"))
@@ -54,16 +60,25 @@ public class MongoDBProvider implements ConnectionProvider<MongoClient> {
         getDatastore().ensureIndexes();
     }
 
+    /**
+     * @return MongoClient instance
+     */
     @Override
     public MongoClient getConnectionInstance() {
         return client;
     }
 
+    /**
+     * @return true if MongoDB is running
+     */
     @Override
     public boolean isRunning() {
         return client != null;
     }
 
+    /**
+     * Disconnects MongoDB
+     */
     @Override @Synchronized
     public void disconnect() {
         client.close();
