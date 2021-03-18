f3d8467eebeb366c8ecb861061ff5e3972d4534e
mluizaa00 <taeckls@gmail.com>
2021-03-18 16:00:24 -0300

    Changed register by object to Reflection on RedisProvider

--- redis-provider/src/main/java/com/celeste/redis/RedisProvider.java
+++ redis-provider/src/main/java/com/celeste/redis/RedisProvider.java
@@ -2,6 +2,8 @@ package com.celeste.redis;
 
 import com.celeste.redis.annotation.Channel;
 import lombok.Getter;
+import lombok.SneakyThrows;
+import org.reflections.Reflections;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPubSub;
 
@@ -20,24 +22,28 @@ public final class RedisProvider {
 
     public void connect(final Properties properties, final boolean credentials) {
         provider.connect(properties, credentials);
+        if (provider.isRunning()) register();
     }
 
-    public void register(final Object... objects) {
-        for (Object object : objects) {
-            final Channel annotation = object.getClass().getAnnotation(Channel.class);
-            if (annotation == null) continue;
-
+    private void register() {
+        final Reflections reflections = new Reflections("com.celeste");
+        for (Class<?> clazz : reflections.getTypesAnnotatedWith(Channel.class)) {
+            final Channel annotation = clazz.getAnnotation(Channel.class);
             registeredChannels.put(annotation.name(), annotation.value());
 
-            new Thread(() -> provider.getConnectionInstance()
-                    .subscribe((JedisPubSub) object, annotation.value())).start();
-
-            return;
+            new Thread(new Runnable() {
+                @SneakyThrows
+                @Override
+                public void run() {
+                    provider.getConnectionInstance()
+                            .subscribe((JedisPubSub) clazz.newInstance(), annotation.value());
+                }
+            }).start();
         }
     }
 
     public void publish(final String message, final String channel) {
-        try (final Jedis jedis = provider.getConnectionInstance()) {
+        try (Jedis jedis = provider.getConnectionInstance()) {
             jedis.publish(registeredChannels.get(channel), message);
         }
     }
