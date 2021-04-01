e3bb731dda18e3ee30e6131ab807aa9618826d5e
DeserRC <DeserRC@gmail.com>
2021-04-01 04:43:33 -0300

    Added method subscribeAll

--- messenger/src/main/java/com/celeste/database/messenger/model/dao/rabbitmq/RabbitMQDAO.java
+++ messenger/src/main/java/com/celeste/database/messenger/model/dao/rabbitmq/RabbitMQDAO.java
@@ -1,38 +1,19 @@
 package com.celeste.database.messenger.model.dao.rabbitmq;
 
-import com.celeste.database.messenger.annotation.Subscribe;
-import com.celeste.database.messenger.model.AbstractListener;
 import com.celeste.database.messenger.model.dao.MessengerDAO;
 import com.celeste.database.messenger.model.database.provider.rabbitmq.RabbitMQ;
-import com.celeste.messenger.model.*;
-import com.celeste.database.shared.model.dao.exception.DAOException;
 import com.celeste.database.shared.model.database.provider.exception.FailedConnectionException;
 import com.rabbitmq.client.Channel;
 import lombok.Getter;
-import lombok.SneakyThrows;
 import org.jetbrains.annotations.NotNull;
-import org.reflections.Reflections;
 
 @Getter
 public class RabbitMQDAO implements MessengerDAO {
 
   private final RabbitMQ database;
 
-  @SneakyThrows({ IllegalAccessException.class, InstantiationException.class })
-  public RabbitMQDAO(@NotNull final RabbitMQ database) throws DAOException, FailedConnectionException {
-    try {
-      this.database = database;
-    } catch (Throwable throwable) {
-      throw new DAOException(throwable);
-    }
-
-    for (final Class<? extends AbstractListener> clazz : new Reflections("").getSubTypesOf(AbstractListener.class)) {
-      final Subscribe annotation = clazz.getAnnotation(Subscribe.class);
-
-      if (annotation == null) continue;
-
-      subscribe(clazz.newInstance(), annotation.value());
-    }
+  public RabbitMQDAO(@NotNull final RabbitMQ database) {
+    this.database = database;
   }
 
   @Override
--- messenger/src/main/java/com/celeste/database/messenger/model/dao/redis/RedisDAO.java
+++ messenger/src/main/java/com/celeste/database/messenger/model/dao/redis/RedisDAO.java
@@ -1,15 +1,10 @@
 package com.celeste.database.messenger.model.dao.redis;
 
-import com.celeste.database.messenger.annotation.Subscribe;
-import com.celeste.database.messenger.model.AbstractListener;
 import com.celeste.database.messenger.model.dao.MessengerDAO;
 import com.celeste.database.messenger.model.database.provider.redis.Redis;
-import com.celeste.database.shared.model.dao.exception.DAOException;
 import com.celeste.database.shared.model.database.provider.exception.FailedConnectionException;
 import lombok.Getter;
-import lombok.SneakyThrows;
 import org.jetbrains.annotations.NotNull;
-import org.reflections.Reflections;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPubSub;
 
@@ -18,21 +13,8 @@ public class RedisDAO implements MessengerDAO {
 
   private final Redis database;
 
-  @SneakyThrows({ IllegalAccessException.class, InstantiationException.class })
-  public RedisDAO(@NotNull final Redis database) throws DAOException, FailedConnectionException {
-    try {
-      this.database = database;
-    } catch (Throwable throwable) {
-      throw new DAOException(throwable);
-    }
-
-    for (final Class<? extends AbstractListener> clazz : new Reflections("").getSubTypesOf(AbstractListener.class)) {
-      final Subscribe annotation = clazz.getAnnotation(Subscribe.class);
-
-      if (annotation == null) continue;
-
-      subscribe(clazz.newInstance(), annotation.value());
-    }
+  public RedisDAO(@NotNull final Redis database) {
+    this.database = database;
   }
 
   @Override
