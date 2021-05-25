1e66acb9b7b637c80c6fe39f564c276b7960d11f
mluizaa00 <luiza.prestes@uol.com.br>
2021-05-25 08:24:49 -0300

    added: more methods to the Cache

--- cache/src/main/java/com/celeste/databases/cache/model/database/dao/CacheDao.java
+++ cache/src/main/java/com/celeste/databases/cache/model/database/dao/CacheDao.java
@@ -3,10 +3,24 @@ package com.celeste.databases.cache.model.database.dao;
 import com.celeste.databases.core.model.database.dao.Dao;
 import com.celeste.databases.core.model.database.provider.exception.FailedConnectionException;
 
+import java.util.Map;
+
 public interface CacheDao extends Dao {
 
   String set(final String key, final String value) throws FailedConnectionException;
 
+  Long set(final String key, final String field, final String value) throws FailedConnectionException;
+
+  String setExpiring(final String key, final int seconds, final String value) throws FailedConnectionException;
+
   void delete(final String key) throws FailedConnectionException;
+
+  void delete(final String key, final String field) throws FailedConnectionException;
+
+  void delete(final String key, final String... fields) throws FailedConnectionException;
+
+  Map<String, String> getAll(final String key) throws FailedConnectionException;
+
+  boolean exists(final String... keys) throws FailedConnectionException;
 
 }
--- cache/src/main/java/com/celeste/databases/cache/model/database/dao/impl/redis/RedisDao.java
+++ cache/src/main/java/com/celeste/databases/cache/model/database/dao/impl/redis/RedisDao.java
@@ -5,6 +5,8 @@ import com.celeste.databases.cache.model.database.provider.impl.redis.Redis;
 import com.celeste.databases.core.model.database.provider.exception.FailedConnectionException;
 import redis.clients.jedis.Jedis;
 
+import java.util.Map;
+
 public final class RedisDao extends AbstractCacheDao<Redis> {
 
   public RedisDao(final Redis cache) {
@@ -21,9 +23,63 @@ public final class RedisDao extends AbstractCacheDao<Redis> {
   }
 
   @Override
+  public Long set(final String key, final String field, final String value) throws FailedConnectionException {
+    try (final Jedis jedis = getDatabase().getJedis()) {
+      return jedis.hset(key, field, value);
+    } catch (Exception exception) {
+      throw new FailedConnectionException(exception);
+    }
+  }
+
+  @Override
+  public String setExpiring(final String key, final int seconds, final String value) throws FailedConnectionException {
+    try (final Jedis jedis = getDatabase().getJedis()) {
+      return jedis.setex(key, seconds, value);
+    } catch (Exception exception) {
+      throw new FailedConnectionException(exception);
+    }
+  }
+
+  @Override
   public void delete(final String key) throws FailedConnectionException {
     try (final Jedis jedis = getDatabase().getJedis()) {
       jedis.del(key);
+    } catch (Exception exception) {
+      throw new FailedConnectionException(exception);
+    }
+  }
+
+  @Override
+  public void delete(final String key, final String field) throws FailedConnectionException {
+    try (final Jedis jedis = getDatabase().getJedis()) {
+      jedis.hdel(key, field);
+    } catch (Exception exception) {
+      throw new FailedConnectionException(exception);
+    }
+  }
+
+  @Override
+  public void delete(final String key, final String... fields) throws FailedConnectionException {
+    try (final Jedis jedis = getDatabase().getJedis()) {
+      jedis.hdel(key, fields);
+    } catch (Exception exception) {
+      throw new FailedConnectionException(exception);
+    }
+  }
+
+  @Override
+  public Map<String, String> getAll(final String key) throws FailedConnectionException {
+    try (final Jedis jedis = getDatabase().getJedis()) {
+      return jedis.hgetAll(key);
+    } catch (Exception exception) {
+      throw new FailedConnectionException(exception);
+    }
+  }
+
+  @Override
+  public boolean exists(final String... key) throws FailedConnectionException {
+    try (final Jedis jedis = getDatabase().getJedis()) {
+      return jedis.exists(key) > 0;
     } catch (Exception exception) {
       throw new FailedConnectionException(exception);
     }
