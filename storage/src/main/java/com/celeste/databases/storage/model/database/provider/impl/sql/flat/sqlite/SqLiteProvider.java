b2b657364adb5dc1ffa7c403f567931dbed05119
DeserRC <DeserRC@gmail.com>
2021-07-08 22:15:18 -0300

    Fixed a error where h2 started instead of sqlite

--- storage/src/main/java/com/celeste/databases/storage/model/database/provider/impl/sql/flat/sqlite/SqLiteProvider.java
+++ storage/src/main/java/com/celeste/databases/storage/model/database/provider/impl/sql/flat/sqlite/SqLiteProvider.java
@@ -10,16 +10,18 @@ import java.sql.Connection;
 import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import org.h2.jdbc.JdbcConnection;
+import org.sqlite.jdbc4.JDBC4Connection;
 
 public final class SqLiteProvider implements Sql {
 
   private static final String URI;
+  private static final String FILE;
   private static final Pattern PATH;
   private static final Pattern NAME;
 
   static {
-    URI = "jdbc:h2:<path>/<name>.db";
+    URI = "jdbc:h2:<path>";
+    FILE = "<name>.db";
     PATH = Pattern.compile("<path>", Pattern.LITERAL);
     NAME = Pattern.compile("<name>", Pattern.LITERAL);
   }
@@ -38,11 +40,13 @@ public final class SqLiteProvider implements Sql {
     try {
       Class.forName("org.sqlite.jdbc4.JDBC4Connection");
 
-      final String newUri = NAME.matcher(PATH.matcher(URI)
-          .replaceAll(Matcher.quoteReplacement(credentials.getPath().getAbsolutePath())))
+      final String newUri = PATH.matcher(URI)
+          .replaceAll(Matcher.quoteReplacement(credentials.getPath().getAbsolutePath()));
+
+      final String newFile = NAME.matcher(FILE)
           .replaceAll(Matcher.quoteReplacement(credentials.getName()));
 
-      final Connection closableConnection = new JdbcConnection(newUri, new Properties());
+      final Connection closableConnection = new JDBC4Connection(newUri, newFile, new Properties());
       this.connection = new NonClosableConnection(closableConnection);
     } catch (Exception exception) {
       throw new FailedConnectionException(exception);
