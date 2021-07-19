8b322a0cdf076e5ea927c9ebc4fb004e1b60fde5
DeserRC <DeserRC@gmail.com>
2021-07-19 17:18:59 -0300

    Most Abstraction

--- storage/src/main/java/com/celeste/databases/storage/model/database/dao/AbstractStorageDao.java
+++ storage/src/main/java/com/celeste/databases/storage/model/database/dao/AbstractStorageDao.java
@@ -1,20 +1,18 @@
 package com.celeste.databases.storage.model.database.dao;
 
+import com.celeste.databases.core.model.database.dao.exception.ValueNotFoundException;
 import com.celeste.databases.core.model.database.provider.Database;
+import com.celeste.databases.core.model.database.provider.exception.FailedConnectionException;
 import com.celeste.databases.storage.model.entity.Data;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public abstract class AbstractStorageDao<T extends Database, U> implements StorageDao<U> {
-
-  protected static final ExecutorService EXECUTOR;
-
-  static {
-    EXECUTOR = new ThreadPoolExecutor(0, Integer.MAX_VALUE, 5L, TimeUnit.MINUTES,
-        new SynchronousQueue<>());
-  }
 
   protected final T storage;
   protected final Class<U> clazz;
@@ -37,6 +35,89 @@ public abstract class AbstractStorageDao<T extends Database, U> implements Stora
   @Override
   public T getDatabase() {
     return storage;
+  }
+
+  @Override
+  public CompletableFuture<Void> saveAsync(final U... entities) {
+    return CompletableFuture.runAsync(() -> {
+      try {
+        save(entities);
+      } catch (FailedConnectionException exception) {
+        exception.printStackTrace();
+      }
+    });
+  }
+
+  @Override
+  public CompletableFuture<Void> saveAsync(final Collection<U> entities) {
+    return CompletableFuture.runAsync(() -> {
+      try {
+        save(entities);
+      } catch (FailedConnectionException exception) {
+        exception.printStackTrace();
+      }
+    });
+  }
+
+  @Override
+  public CompletableFuture<Void> deleteAsync(final U... entities) {
+    return CompletableFuture.runAsync(() -> {
+      try {
+        delete(entities);
+      } catch (FailedConnectionException exception) {
+        exception.printStackTrace();
+      }
+    });
+  }
+
+  @Override
+  public CompletableFuture<Void> deleteAsync(final Collection<U> entities) {
+    return CompletableFuture.runAsync(() -> {
+      try {
+        delete(entities);
+      } catch (FailedConnectionException exception) {
+        exception.printStackTrace();
+      }
+    });
+  }
+
+  @Override
+  public CompletableFuture<Boolean> containsAsync(final Object key) {
+    return CompletableFuture.supplyAsync(() -> {
+      try {
+        return contains(key);
+      } catch (FailedConnectionException exception) {
+        exception.printStackTrace();
+      }
+
+      return null;
+    });
+  }
+
+  @Override
+  public CompletableFuture<U> findAsync(final Object key) {
+    return CompletableFuture.supplyAsync(() -> {
+      try {
+        return find(key);
+      } catch (FailedConnectionException | ValueNotFoundException exception) {
+        exception.printStackTrace();
+      }
+
+      return null;
+    });
+  }
+
+  @Override
+  public CompletableFuture<List<U>> findAllAsync() {
+    return CompletableFuture.supplyAsync(() -> {
+      try {
+        return findAll();
+      } catch (FailedConnectionException exception) {
+        exception.printStackTrace();
+      }
+
+      return null;
+    });
   }
 
 }
